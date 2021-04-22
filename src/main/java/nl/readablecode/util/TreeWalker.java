ac868595e36aaff1826ff03cfb31419dc0738bc9
Florimon van Putte <florimon@florimon.net>
2021-04-23 01:28:32 +0200

    refactored to use recursion

--- src/main/java/nl/readablecode/util/TreeWalker.java
+++ src/main/java/nl/readablecode/util/TreeWalker.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
 
 import static java.util.Optional.ofNullable;
 
@@ -26,10 +25,6 @@ public class TreeWalker<T> {
     /** A Predicate that will be called with each visited node, until it returns <code>true</code>. */
     private final Predicate<T> visitor;
 
-    private UnaryOperator<T> operator() {
-        return t -> visitor.test(t) ? t : null;
-    }
-
     /**
      * Performs a depth-first 'walk' over the tree represented by the given root nodes.
      *
@@ -38,17 +33,11 @@ public class TreeWalker<T> {
      *              if no node satisfies the predicate
      */
     public T depthFirst(Collection<T> roots) {
-        for (T root : roots) {
-            if (visitor.test(root)) {
-                return root;
-            } else {
-                T t = depthFirst(traverser.apply(root));
-                if (t != null) {
-                    return t;
-                }
-            }
-        }
-        return null;
+        return findFirst(roots, this::depthFirst).orElse(null);
+    }
+
+    private T depthFirst(T root) {
+        return ofNullable(returnIfMatches(root)).orElseGet(() -> depthFirst(traverser.apply(root)));
     }
 
     /**
@@ -59,34 +48,15 @@ public class TreeWalker<T> {
      *              if no node satisfies the predicate
      */
     public T breadthFirst(Collection<T> roots) {
-        for (T root : roots) {
-            if (visitor.test(root)) {
-                return root;
-            }
-        }
-        for (T root : roots) {
-            T t = breadthFirst(traverser.apply(root));
-            if (t != null) {
-                return t;
-            }
-        }
-        return null;
+        return findFirst(roots, this::returnIfMatches).orElseGet(() ->
+                findFirst(roots, traverser.andThen(this::breadthFirst)).orElse(null));
     }
 
-    public T depthFirst2(Collection<T> roots) {
-        return findFirst(roots, this::depthFirst2).orElse(null);
+    private T returnIfMatches(T t) {
+        return visitor.test(t) ? t : null;
     }
 
-    private T depthFirst2(T root) {
-        return ofNullable(operator().apply(root)).orElseGet(() -> depthFirst2(traverser.apply(root)));
-    }
-
-    public T breadthFirst2(Collection<T> roots) {
-        return findFirst(roots, operator()).orElseGet(() ->
-                findFirst(roots, root -> breadthFirst2(traverser.apply(root))).orElse(null));
-    }
-
-    private Optional<T> findFirst(Collection<T> collection, UnaryOperator<T> operator) {
-        return collection.stream().map(operator).filter(Objects::nonNull).findFirst();
+    private Optional<T> findFirst(Collection<T> collection, Function<T,T> matcher) {
+        return collection.stream().map(matcher).filter(Objects::nonNull).findFirst();
     }
 }
