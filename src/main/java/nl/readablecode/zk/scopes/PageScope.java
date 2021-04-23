9cf847fde824b39d79f360b0fd96f8b153aa6af0
Florimon van Putte <florimon@florimon.net>
2021-04-23 21:21:09 +0200

    Added javadoc

--- src/main/java/nl/readablecode/zk/scopes/PageScope.java
+++ src/main/java/nl/readablecode/zk/scopes/PageScope.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import java.lang.annotation.*;
 
 /**
+ * A {@link Scope}-derived meta-annotation to indicate that the annotated bean should be created in/taken from
+ * the ZK-specific 'page' scope.
  *
+ * @author florimon
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
