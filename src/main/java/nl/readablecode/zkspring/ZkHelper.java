19c59e1cc7d5a27aede2dca957e81c350b87f2aa
Florimon van Putte <florimon.van.putte@nl.abnamro.com>
2021-09-24 16:50:20 +0200

    Added methods to bind to all events (except mouse) for testing, and method to bind to multiple specific events

--- src/main/java/nl/readablecode/zkspring/ZkHelper.java
+++ src/main/java/nl/readablecode/zkspring/ZkHelper.java
@@ -2,6 +2,8 @@ package nl.readablecode.zkspring;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ReflectionUtils;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Page;
@@ -12,10 +14,14 @@ import org.zkoss.zk.ui.impl.PollingServerPush;
 import org.zkoss.zk.ui.sys.DesktopCtrl;
 import org.zkoss.zk.ui.util.ConventionWires;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.emptyMap;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ZkHelper {
     private static final String ZUL_PREFIX = "~.";
@@ -60,5 +66,40 @@ public final class ZkHelper {
 
     public static boolean onChange(Component component, Runnable runnable) {
         return component.addEventListener(Events.ON_CHANGE, event -> runnable.run());
+    }
+
+    public static void onEvents(Component component, Runnable runnable, String... eventNames) {
+        onEvents(component, event -> runnable.run(), eventNames);
+    }
+
+    public static void onEvents(Component component, EventListener<? extends Event> listener, String... eventNames) {
+        Arrays.stream(eventNames).forEach(eventName -> component.addEventListener(eventName, listener));
+    }
+
+    public static boolean onAll(Component component, Runnable runnable) {
+        return onAll(component, event -> runnable.run());
+    }
+
+    public static boolean onAll(Component component, EventListener<? extends Event> listener) {
+        getAllEventNames().stream().filter(eventName -> !eventName.startsWith("onMouse"))
+                        .forEach(eventName -> addOrIgnoreEventListener(component, eventName, listener));
+        return true;
+    }
+
+    public static boolean addOrIgnoreEventListener(Component component, String eventName, EventListener<? extends Event> listener) {
+        try {
+            return component.addEventListener(eventName, listener);
+        } catch (IllegalArgumentException e) {
+            log.error("Failed to add listener for event '{}'", eventName);
+            return false;
+        }
+    }
+
+    public static List<String> getAllEventNames() {
+        List<String> eventNames = new ArrayList<>();
+        ReflectionUtils.doWithFields(Events.class,
+                field -> eventNames.add((String) ReflectionUtils.getField(field, null)),
+                field -> field.getName().startsWith("ON_"));
+        return eventNames;
     }
 }
